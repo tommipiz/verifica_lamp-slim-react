ee2b921a020d19589421c69f463e47c7d8b6d3c6
Tommaso Pizzutilo <tommaso.pizzutilo@gmail.com>
2025-04-14 13:14:42 +0200

    aggiustamenti a index.php

--- php/index.php
+++ php/index.php
@@ -11,38 +11,144 @@ $app = AppFactory::create();
 
 //Classi
 
-//curl http://localhost:8080/classi
+
+/*
+curl http://localhost:8080/classi
+Richiesta:
+GET    /classi             
+Risposta
+Status code: 200   
+[{"id": 1, "sezione":"5A", "anno": 2024},  {"id": 2 , "sezione":"5B", "anno": 2024}]
+*/
 $app->get('/classi', "ClassiController:index");
 
-//curl http://localhost:8080/classi/2
+
+
+
+/*
+curl http://localhost:8080/classi/2
+Richiesta:
+GET    /classi/2            
+Risposta
+Status code: 200   
+[{"id": 2 , "sezione":"5B", "anno": 2024}]
+*/
+
 $app->get('/classi/{id}', "ClassiController:show");
 
-//curl -X POST http://localhost:8080/classi -H "Contenct-Type: application/json" -d '{"id":"1","sezione":"5A","anno":"2024"}'
+
+
+
+/*
+curl -X POST http://locahost:8080/classi -H "Content-Type: application/json" -d '{"sezione": "5C", "anno": 2025}
+Richiesta:
+POST    /classi             
+Risposta
+Status code: 201   
+{"msg": "creato"}
+*/
 $app->post('/classi', "ClassiController:create");
 
-//curl -X PUT http://localhost:8080/classi/2 -H "Contenct-Type: application/json" -d '{"id":"1","sezione":"5A","anno":"2024"}'
+
+
+
+
+/*
+curl -X PUT http://localhost:8080/classi/2 -H "Contenct-Type: application/json" -d '{"id":"3","sezione":"5C","anno":"2024"}'
+Richiesta:
+POST    /classi/2             
+Risposta
+Status code: 201   
+{"msg": "aggiornato"}
+
+*/
 $app->put('/classi/{id}', "ClassiController:update");
 
-//curl -X DELETE http://localhost:8080/classi/2
+
+
+
+
+/*
+curl -X DELETE http://localhost:8080/classi/2
+Richiesta:
+POST    /classi/2             
+Risposta
+Status code: 201   
+{"msg": "eliminato"}
+*/
 $app->delete('/classi/{id}', "ClassiController:destroy");
 
-
+//-----------------------------------------------------------------------------------------
 //Alunni
 
-//curl http://localhost:8080/classi/2/alunni
+
+
+/*
+curl http://localhost:8080/classi/1/alunni
+Richiesta:
+GET    /classi/1/alunni    
+Risposta
+Status code: 200   
+[{"id": 1, "nome":"Claudio", "Cognome": "Benvenuti"},  {"id": 1 , "nome":"Ivan", "cognome": "Bruno"}]
+*/
 $app->get('/classi/{id}/alunni', "AlunniController:index");
 
-//curl http://localhost:8080/alunni/2
+
+
+
+
+/*
+curl  http://localhost:8080/alunni/1
+Richiesta:
+GET    /classi/1   
+Risposta
+Status code: 200   
+[{"id": 1, "nome":"Claudio", "Cognome": "Benvenuti"},  {"id": 1 , "nome":"Ivan", "cognome": "Bruno"}]
+*/
 $app->get('/alunni/{id}', "AlunniController:show");
 
-//curl -X POST http://localhost:8080/classi/2/alunni -H "Contenct-Type: application/json" -d '{"id":"2","nome":"Ivan","cognome":"Bruno","classe_id":"1"}'
+
+
+
+/*
+curl -X POST http://localhost:8080/classi/1/alunni -H "Contenct-Type: application/json" -d '{"id":"1","nome":"Ivan","cognome":"Bruno","classe_id":"2"}'
+Richiesta:
+POST    /classi/1/alunni           
+Risposta
+Status code: 201   
+{"msg": "creato"}
+*/
 $app->post('/classi/{id}/alunni', "AlunniController:create");
 
-//curl -X PUT http://localhost:8080/alunni/2 -H "Contenct-Type: application/json" -d '{"id":"2","nome":"Ivan","cognome":"Bruno","classe_id":"1"}' 
+
+
+
+
+/*
+curl -X PUT http://localhost:8080/alunni/1 -H "Contenct-Type: application/json" -d '{"id":"1","nome":"Ivan","cognome":"Bruno","classe_id":"2"}' 
+Richiesta:
+POST    /alunni/1            
+Risposta
+Status code: 201   
+{"msg": "aggiornato"}
+*/
 $app->put('/alunni/{id}', "AlunniController:update");
 
-//curl -X DELETE http://localhost:8080/alunni/2
+
+
+
+
+/*
+curl -X DELETE http://localhost:8080/alunni/1
+Richiesta:
+POST    /classi/1            
+Risposta
+Status code: 201   
+{"msg": "eliminato"}
+*/
 $app->delete('/alunni/{id}', "AlunniController:destroy");
+
+
 
 
 
